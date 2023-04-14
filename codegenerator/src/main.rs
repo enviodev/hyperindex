785939fac534b3cc47c90445428591e10d43531d
Jason <Jason@wildcards.world>
2023-04-14 15:54:11 +0200

    Most basic CLI to specify codegen paths using CLAP
    Squashed commit of the following:
    
    commit 065eff54688a6813e5e72f1666622e873610ab65
    Author: Jason <Jason@wildcards.world>
    Date:   Fri Apr 14 15:52:28 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:codegenerator/src/main.rs
    
    commit a042783f32b7bee76ce80ce5c1fc05063e82dcff
    Author: WooSungD <woosung@float.capital>
    Date:   Fri Apr 14 14:42:01 2023 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:scenarios/test_codegen/generated/src/EventSyncing.res
    
    commit 2786e8f4a04a77241a9d286666897a7c2b081a76
    Author: Mike Young <mj.young114@gmail.com>
    Date:   Fri Apr 14 15:17:40 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:codegenerator/src/main.rs
    Co-authored-by: WooSungD <woosung@float.capital>
    Co-authored-by: Mike Young <mj.young114@gmail.com>

--- codegenerator/src/main.rs
+++ codegenerator/src/main.rs
@@ -1,33 +1,57 @@
 use std::error::Error;
+use std::path::PathBuf;
 use std::process::Command;
+
+use clap::Parser;
 
 use rust_code_gen::{
     config_parsing, copy_directory, entity_parsing, event_parsing, generate_templates,
 };
 
-const CODE_GEN_PATH: &str = "../scenarios/test_codegen/generated";
-const PROJECT_ROOT_PATH: &str = "../scenarios/test_codegen";
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+
+struct Args {
+    #[arg(short, long, default_value_t=String::from("./"))]
+    directory: String,
+
+    #[arg(short, long, default_value_t=String::from("generated/"))]
+    output_directory: String,
+
+    #[arg(short, long, default_value_t=String::from("config.yaml"))]
+    config: String,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    copy_directory("templates/static", CODE_GEN_PATH)?;
-    let config = config_parsing::get_config_from_yaml(PROJECT_ROOT_PATH)?;
-    let contract_types = event_parsing::get_contract_types_from_config(PROJECT_ROOT_PATH, &config)?;
-    let entity_types = entity_parsing::get_entity_record_types_from_schema(PROJECT_ROOT_PATH)?;
+    let args = Args::parse();
+
+    let project_root_path_buf = PathBuf::from(args.directory);
+    let mut code_gen_path_buf = project_root_path_buf.clone();
+
+    code_gen_path_buf.push(args.output_directory);
+
+    let code_gen_path = code_gen_path_buf.to_str().unwrap();
+    let project_root_path = project_root_path_buf.to_str().unwrap();
+
+    copy_directory("templates/static", code_gen_path)?;
+    let config = config_parsing::get_config_from_yaml(project_root_path)?;
+    let contract_types = event_parsing::get_contract_types_from_config(project_root_path, &config)?;
+    let entity_types = entity_parsing::get_entity_record_types_from_schema(project_root_path)?;
     let chain_config_templates =
-        config_parsing::convert_config_to_chain_configs(&config, PROJECT_ROOT_PATH)?;
+        config_parsing::convert_config_to_chain_configs(&config, project_root_path)?;
 
     generate_templates(
         contract_types,
         chain_config_templates,
         entity_types,
-        CODE_GEN_PATH,
+        code_gen_path,
     )?;
 
     println!("installing packages... ");
 
     Command::new("pnpm")
         .arg("install")
-        .current_dir(CODE_GEN_PATH)
+        .current_dir(code_gen_path)
         .spawn()?
         .wait()?;
 
@@ -37,7 +61,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .arg("rescript")
         .arg("format")
         .arg("-all")
-        .current_dir(CODE_GEN_PATH)
+        .current_dir(code_gen_path)
         .spawn()?
         .wait()?;
 
@@ -45,7 +69,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Command::new("pnpm")
         .arg("build")
-        .current_dir(CODE_GEN_PATH)
+        .current_dir(code_gen_path)
         .spawn()?
         .wait()?;
 
