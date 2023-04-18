ac616671c7bec1f80ed39c975aec5e2ad383805e
Jason <Jason@wildcards.world>
2023-04-18 23:02:20 +0200

    Update script to make the created tables public in hasura

--- codegenerator/templates/dynamic/register_tables_with_hasura.sh
+++ codegenerator/templates/dynamic/register_tables_with_hasura.sh
@@ -2,6 +2,7 @@
 #       only implemented like this until we have chance to discuss a more pollished solution.
 
 # NOTE: This is very brittle code, it will break if the password, hasura url or database changes.
+#       Good to see the API though, we could write this in code to be more robust once things have settled a bit more.
 
 {{#each entities as |entity|}}
 curl -X POST localhost:8080/v1/metadata \
@@ -15,5 +16,24 @@ curl -X POST localhost:8080/v1/metadata \
     "schema": "public",
     "name": "{{entity.name.uncapitalized}}"
   }
+}'
+{{/each}}
+
+{{#each entities as |entity|}}
+curl -X POST localhost:8080/v1/metadata \
+  -H "Content-Type: application/json" \
+  -H "X-Hasura-Role: admin" \
+  -H "X-Hasura-Admin-Secret: testing" \
+  -d '{
+    "type": "pg_create_select_permission",
+    "args": {
+        "table": "{{entity.name.uncapitalized}}",
+        "role": "public",
+        "source": "default",
+        "permission": {
+            "columns": "*",
+            "filter": {}
+        }
+    }
 }'
 {{/each}}
