1919b7911fb4ddbf64851f3a6b43aac744e72d03
Mike Young <mike@float.capital>
2023-05-22 09:40:01 +0200

    Fix NFT scenario to use hardhat to deploy

--- scenarios/nft-factory/hardhat.config.ts
+++ scenarios/nft-factory/hardhat.config.ts
@@ -1,8 +1,13 @@
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+import "hardhat-abi-exporter";
 import { HardhatUserConfig } from "hardhat/config";
 
 import "@typechain/hardhat";
+import "./contracts/tasks";
 import { TypechainUserConfig } from "@typechain/hardhat/dist/types";
+
+import { mnemonic } from "./secretsManager";
 
 let typeChainConfig: TypechainUserConfig = {
   target: "ethers-v6",
@@ -15,12 +20,29 @@ const config: HardhatUserConfig = {
   },
   typechain: typeChainConfig,
   networks: {
+    ganache: {
+      url: "http://0.0.0.0:8545",
+      chainId: 1337,
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
+      chainId: 1337,
       accounts: {
         mnemonic: "test test test test test test test test test test test junk",
       },
     },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: { mnemonic },
+    },
+  },
+  abiExporter: {
+    path: "./abis",
+    clear: true,
+    flat: true,
+    spacing: 2,
   },
 };
 
