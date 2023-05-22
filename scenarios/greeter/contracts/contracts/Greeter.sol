12a71575c31860444fc890ebda8ff5222304c978
denhampreen <denhampreen@gmail.com>
2023-05-22 14:13:12 +0200

    adjust: abi

--- scenarios/greeter/contracts/contracts/Greeter.sol
+++ scenarios/greeter/contracts/contracts/Greeter.sol
@@ -8,6 +8,8 @@ error GreeterError();
 contract Greeter {
     string public greeting;
 
+    event Greeting(string greeting);
+
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
@@ -20,6 +22,7 @@ contract Greeter {
     function setGreeting(string memory _greeting) public {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
+        emit Greeting(_greeting);
     }
 
     function throwError() external pure {
