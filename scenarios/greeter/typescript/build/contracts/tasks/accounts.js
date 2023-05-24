31a969b81bfc7c5755a542bf9be5d0ed6d44f641
WooSungD <woosung@float.capital>
2023-05-24 12:13:12 +0100

    delete build files

--- scenarios/greeter/typescript/build/contracts/tasks/accounts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("hardhat/config");
-(0, config_1.task)("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-    for (const account of accounts) {
-        console.log(await account.getAddress());
-    }
-});
