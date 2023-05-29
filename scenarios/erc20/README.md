d64577cf873de19c2bb7693eaef5f6b3a99486df
WooSungD <woosung@float.capital>
2023-05-29 18:42:55 +0100

    ccommiting rest of config changes

--- scenarios/erc20/README.md
+++ scenarios/erc20/README.md
@@ -1,3 +1,11 @@
+## Open Zepplin ERC-20
+
+Install Open Zepplin contracts by running the following:
+
+```
+npm install @openzeppelin/contracts
+```
+
 ## Indexer Requirements
 
 The following files are required to use the Indexer:
--- scenarios/erc20/src/EventHandlers.ts
+++ scenarios/erc20/src/EventHandlers.ts
@@ -1,52 +1,60 @@
 import {
-  GreeterContract_registerNewGreetingLoadEntities,
-  GreeterContract_registerNewGreetingHandler,
-  GreeterContract_registerClearGreetingLoadEntities,
-  GreeterContract_registerClearGreetingHandler,
+  ERC20Contract_registerCreationHandler,
+  ERC20Contract_registerCreationLoadEntities,
+  ERC20Contract_registerTransferHandler,
+  ERC20Contract_registerTransferLoadEntities,
 } from "../generated/src/Handlers.gen";
 
-import { greetingEntity } from "../generated/src/Types.gen";
+import {
+  erc20TokenEntity,
+  totalTransfersEntity,
+} from "../generated/src/Types.gen";
 
-GreeterContract_registerNewGreetingLoadEntities(({ event, context }) => {
-  context.greeting.greetingWithChangesLoad(event.params.user.toString());
+ERC20Contract_registerCreationLoadEntities(({ event, context }) => {});
+
+ERC20Contract_registerCreationHandler(({ event, context }) => {
+
+  // creating a erc20TokenEntity to store the event data
+  let erc20TokenObject: erc20TokenEntity = {
+    id: event.srcAddress.toString(),
+    name: event.params.name.toString(),
+    symbol: event.params.symbol.toString(),
+    decimals: 18,
+  };
+  
+  // creating a new entry in erc20Token table with the event data
+  context.erc20Token.insert(erc20TokenObject);
+  
+  // creating a totalTransfersEntity to store the event data
+  let totalTransferObject: totalTransfersEntity = {
+    id: event.srcAddress.toString(),
+    erc20: erc20TokenObject,
+    totalTransfer: 0,
+  };
+  
+  // creating a new entry in totalTransfers table with the event data
+  context.totalTransfers.insert(totalTransferObject);
 });
 
-GreeterContract_registerNewGreetingHandler(({ event, context }) => {
-  let currentGreeter = context.greeting.greetingWithChanges();
-
-  if (currentGreeter != null) {
-    let greetingObject: greetingEntity = {
-      id: event.params.user.toString(),
-      latestGreeting: event.params.greeting,
-      numberOfGreetings: currentGreeter.numberOfGreetings + 1,
-    };
-
-    context.greeting.update(greetingObject);
-  } else {
-    let greetingObject: greetingEntity = {
-      id: event.params.user.toString(),
-      latestGreeting: event.params.greeting,
-      numberOfGreetings: 1,
-    };
-    context.greeting.insert(greetingObject);
-  }
+ERC20Contract_registerTransferLoadEntities(({ event, context }) => {
+  // loading the required totalTransfersEntity to update the totalTransfer field
+  context.totalTransfers.totalTransferChangesLoad(event.srcAddress.toString());
 });
 
-GreeterContract_registerClearGreetingLoadEntities(({ event, context }) => {
-  context.greeting.greetingWithChangesLoad(event.params.user.toString());
-});
+ERC20Contract_registerTransferHandler(({ event, context }) => {
+  // getting the current totalTransfer field value
+  let currentTotalTransfer = context.totalTransfers.totalTransferChanges();
 
-GreeterContract_registerClearGreetingHandler(({ event, context }) => {
-  let currentGreeter = context.greeting.greetingWithChanges();
-
-  if (currentGreeter != null) {
-    let greetingObject: greetingEntity = {
-      id: event.params.user.toString(),
-      latestGreeting: "",
-      numberOfGreetings: currentGreeter.numberOfGreetings,
+  if (currentTotalTransfer != null) {
+    // updating the totalTransfer field value
+    let totalTransferObject: totalTransfersEntity = {
+      id: event.srcAddress.toString(),
+      erc20: currentTotalTransfer.erc20,
+      totalTransfer: currentTotalTransfer.totalTransfer + Number(event.params.value),
     };
 
-    context.greeting.update(greetingObject);
+    // updating the totalTransfers table with the new totalTransfer field value
+    context.totalTransfers.update(totalTransferObject);
   } else {
   }
 });
