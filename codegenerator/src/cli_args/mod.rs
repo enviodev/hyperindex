f701cd93d1ea9ee47b5b88b3a7524cec54022a75
WooSungD <woosung@float.capital>
2023-06-02 09:27:39 +0200

    initial changes for blank template

--- codegenerator/src/cli_args/mod.rs
+++ codegenerator/src/cli_args/mod.rs
@@ -92,8 +92,8 @@ pub struct InitArgs {
 ///Template to work off
 pub enum Template {
     Blank,
-    Greeter,
     Erc20,
+    Greeter,
 }
 
 #[derive(Clone, Debug, ValueEnum, Serialize, Deserialize)]
