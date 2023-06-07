c4c79b700487bce6aa69ea759d1121d78465bf3b
WooSungD <woosung@float.capital>
2023-06-07 16:16:07 +0200

    updated ts example

--- scenarios/erc20/typescript/src/EventHandlers.ts
+++ scenarios/erc20/typescript/src/EventHandlers.ts
@@ -1,60 +1,103 @@
 import {
-  ERC20Contract_registerCreationHandler,
-  ERC20Contract_registerCreationLoadEntities,
+  ERC20Contract_registerApprovalLoadEntities,
+  ERC20Contract_registerApprovalHandler,
   ERC20Contract_registerTransferHandler,
   ERC20Contract_registerTransferLoadEntities,
 } from "../generated/src/Handlers.gen";
 
-import {
-  tokensEntity,
-  totalsEntity
-} from "../generated/src/Types.gen";
+import { accountEntity } from "../generated/src/Types.gen";
 
-ERC20Contract_registerCreationLoadEntities(({ event, context }) => {
+ERC20Contract_registerApprovalLoadEntities(({ event, context }) => {
+  // loading the required accountEntity
+  context.account.ownerAccountChangesLoad(event.params.owner.toString());
 });
 
-ERC20Contract_registerCreationHandler(({ event, context }) => {
-  // creating a erc20TokenEntity to store the event data
-  let tokenObject: tokensEntity = {
-    id: event.srcAddress.toString(),
-    name: event.params.name.toString(),
-    symbol: event.params.symbol.toString(),
-    decimals: 18,
-  };
+ERC20Contract_registerApprovalHandler(({ event, context }) => {
+  //  getting the owner accountEntity
+  let ownerAccount = context.account.ownerAccountChanges();
 
-  // creating a new entry in erc20Token table with the event data
-  context.tokens.insert(tokenObject);
-  
-  // creating a totalsEntity to store the event data
-  let totalsObject: totalsEntity= {
-    id: event.srcAddress.toString(),
-    erc20: tokenObject.id,
-    totalTransfer: BigInt(0),
-  };
-  
-  // creating a new entry in totals table with the event data
-  context.totals.insert(totalsObject);
+  if (ownerAccount != undefined) {
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: ownerAccount.id,
+      approval: event.params.value,
+      balance: ownerAccount.balance,
+    };
+
+    // updating the accountEntity with the new transfer field value
+    context.account.update(accountObject);
+  } else {
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: event.params.owner.toString(),
+      approval: event.params.value,
+      balance: BigInt(0),
+    };
+
+    // inserting the accountEntity with the new transfer field value
+    context.account.insert(accountObject);
+  }
 });
 
 ERC20Contract_registerTransferLoadEntities(({ event, context }) => {
-  // loading the required totalsEntity to update the totals field
-  context.totals.totalChangesLoad(event.srcAddress.toString());
+  // loading the required accountEntity
+  context.account.senderAccountChangesLoad(event.params.from.toString());
+  context.account.receiverAccountChangesLoad(event.params.to.toString());
 });
 
 ERC20Contract_registerTransferHandler(({ event, context }) => {
-  // getting the current totals field value
-  let currentTotalTransfer = context.totals.totalChanges();
+  // getting the sender accountEntity
+  let senderAccount = context.account.senderAccountChanges();
 
-  if (currentTotalTransfer != null) {
+  if (senderAccount != undefined) {
     // updating the totals field value
-    let totalsObject: totalsEntity = {
-      id: event.srcAddress.toString(),
-      erc20: currentTotalTransfer.erc20,
-      totalTransfer: BigInt(Number(currentTotalTransfer.totalTransfer) + Number(event.params.value))
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: senderAccount.id,
+      approval: senderAccount.approval,
+      balance: BigInt(
+        Number(senderAccount.balance) - Number(event.params.value)
+      ),
     };
 
-    // updating the totalTransfers table with the new totals field value
-    context.totals.update(totalsObject);
+    // updating the accountEntity with the new transfer field value
+    context.account.update(accountObject);
   } else {
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: event.params.from.toString(),
+      approval: BigInt(0),
+      balance: BigInt(0 - Number(event.params.value)),
+    };
+
+    // inserting the accountEntity with the new transfer field value
+    context.account.insert(accountObject);
+  }
+
+  // getting the sender accountEntity
+  let receiverAccount = context.account.receiverAccountChanges();
+
+  if (receiverAccount != undefined) {
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: receiverAccount.id,
+      approval: receiverAccount.approval,
+      balance: BigInt(
+        Number(receiverAccount.balance) + Number(event.params.value)
+      ),
+    };
+
+    // updating the accountEntity with the new transfer field value
+    context.account.update(accountObject);
+  } else {
+    // updating accountEntity object
+    let accountObject: accountEntity = {
+      id: event.params.to.toString(),
+      approval: BigInt(0),
+      balance: event.params.value,
+    };
+
+    // inserting the accountEntity with the new transfer field value
+    context.account.insert(accountObject);
   }
 });
