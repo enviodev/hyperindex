736ce4f8d15ba5ae57b6dd4346271e9880cc9703
Jason <Jason@wildcards.world>
2023-06-07 09:23:46 +0200

    Create schema and config for testing entity loading

--- scenarios/test_codegen/generated/src/DbFunctionsImplementation.js
+++ scenarios/test_codegen/generated/src/DbFunctionsImplementation.js
@@ -1,4 +1,4 @@
-// db operations for raw_events:
+ // db operations for raw_events:
 
 module.exports.readLatestRawEventsBlockNumberProcessedOnChainId = (
   sql,
@@ -365,9 +365,9 @@ FROM public.token
 WHERE id IN ${sql(entityIdArray)};`
 // end db operations for Token
 
-  // db operations for A:
+// db operations for A:
 
-  module.exports.readAEntities = (sql, entityIdArray) => sql`
+module.exports.readAEntities = (sql, entityIdArray) => sql`
   SELECT 
   "id",
   "b",
@@ -376,7 +376,7 @@ WHERE id IN ${sql(entityIdArray)};`
   FROM public.a
   WHERE id IN ${sql(entityIdArray)}`
 
-  const batchSetACore = (sql, entityDataArray) => {
+const batchSetACore = (sql, entityDataArray) => {
   const combinedEntityAndEventData = entityDataArray.map((entityData) => ({
     ...entityData.entity,
     ...entityData.eventData,
@@ -396,24 +396,24 @@ WHERE id IN ${sql(entityIdArray)};`
     "event_chain_id" = EXCLUDED."event_chain_id",
     "event_id" = EXCLUDED."event_id"
   ;`
-  }
+}
 
-  module.exports.batchSetA = (sql, entityDataArray) => {
-    // TODO: make this max batch size optimal. Do calculations to achieve this.
-    const MAX_ITEMS_PER_QUERY_A = 50;
+module.exports.batchSetA = (sql, entityDataArray) => {
+  // TODO: make this max batch size optimal. Do calculations to achieve this.
+  const MAX_ITEMS_PER_QUERY_A = 50;
 
-    return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_A, batchSetACore);
-  }
+  return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_A, batchSetACore);
+}
 
-  module.exports.batchDeleteA = (sql, entityIdArray) => sql`
+module.exports.batchDeleteA = (sql, entityIdArray) => sql`
   DELETE
   FROM public.a
   WHERE id IN ${sql(entityIdArray)};`
-  // end db operations for A
+// end db operations for A
 
-  // db operations for B:
+// db operations for B:
 
-  module.exports.readBEntities = (sql, entityIdArray) => sql`
+module.exports.readBEntities = (sql, entityIdArray) => sql`
   SELECT 
   "id",
   "a",
@@ -423,7 +423,7 @@ WHERE id IN ${sql(entityIdArray)};`
   FROM public.b
   WHERE id IN ${sql(entityIdArray)}`
 
-  const batchSetBCore = (sql, entityDataArray) => {
+const batchSetBCore = (sql, entityDataArray) => {
   const combinedEntityAndEventData = entityDataArray.map((entityData) => ({
     ...entityData.entity,
     ...entityData.eventData,
@@ -446,24 +446,24 @@ WHERE id IN ${sql(entityIdArray)};`
     "event_chain_id" = EXCLUDED."event_chain_id",
     "event_id" = EXCLUDED."event_id"
   ;`
-  }
+}
 
-  module.exports.batchSetB = (sql, entityDataArray) => {
-    // TODO: make this max batch size optimal. Do calculations to achieve this.
-    const MAX_ITEMS_PER_QUERY_B = 50;
+module.exports.batchSetB = (sql, entityDataArray) => {
+  // TODO: make this max batch size optimal. Do calculations to achieve this.
+  const MAX_ITEMS_PER_QUERY_B = 50;
 
-    return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_B, batchSetBCore);
-  }
+  return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_B, batchSetBCore);
+}
 
-  module.exports.batchDeleteB = (sql, entityIdArray) => sql`
+module.exports.batchDeleteB = (sql, entityIdArray) => sql`
   DELETE
   FROM public.b
   WHERE id IN ${sql(entityIdArray)};`
-  // end db operations for B
+// end db operations for B
 
-  // db operations for C:
+// db operations for C:
 
-  module.exports.readCEntities = (sql, entityIdArray) => sql`
+module.exports.readCEntities = (sql, entityIdArray) => sql`
   SELECT 
   "id",
   "a",
@@ -472,7 +472,7 @@ WHERE id IN ${sql(entityIdArray)};`
   FROM public.c
   WHERE id IN ${sql(entityIdArray)}`
 
-  const batchSetCCore = (sql, entityDataArray) => {
+const batchSetCCore = (sql, entityDataArray) => {
   const combinedEntityAndEventData = entityDataArray.map((entityData) => ({
     ...entityData.entity,
     ...entityData.eventData,
@@ -492,16 +492,16 @@ WHERE id IN ${sql(entityIdArray)};`
     "event_chain_id" = EXCLUDED."event_chain_id",
     "event_id" = EXCLUDED."event_id"
   ;`
-  }
+}
 
-  module.exports.batchSetC = (sql, entityDataArray) => {
-    // TODO: make this max batch size optimal. Do calculations to achieve this.
-    const MAX_ITEMS_PER_QUERY_C = 50;
+module.exports.batchSetC = (sql, entityDataArray) => {
+  // TODO: make this max batch size optimal. Do calculations to achieve this.
+  const MAX_ITEMS_PER_QUERY_C = 50;
 
-    return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_C, batchSetCCore);
-  }
+  return chunkBatchQuery(sql, entityDataArray, MAX_ITEMS_PER_QUERY_C, batchSetCCore);
+}
 
-  module.exports.batchDeleteC = (sql, entityIdArray) => sql`
+module.exports.batchDeleteC = (sql, entityIdArray) => sql`
   DELETE
   FROM public.c
   WHERE id IN ${sql(entityIdArray)};`
