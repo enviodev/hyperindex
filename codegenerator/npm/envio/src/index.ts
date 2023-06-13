dd97254e159f7b847bf9ffe9c70c2e2be420dd73
Jason <Jason@wildcards.world>
2023-06-13 09:46:26 +0200

    Final touches

--- codegenerator/npm/envio/src/index.ts
+++ codegenerator/npm/envio/src/index.ts
@@ -3,12 +3,12 @@
 import { spawnSync } from "child_process"
 
 /**
-  * Returns the executable path for git-cliff located inside node_modules
-  * The naming convention is git-cliff-${os}-${arch}
+  * Returns the executable path for envio located inside node_modules
+  * The naming convention is envio-${os}-${arch}
   * If the platform is `win32` or `cygwin`, executable will include a `.exe` extension
   * @see https://nodejs.org/api/os.html#osarch
   * @see https://nodejs.org/api/os.html#osplatform
-  * @example "x/xx/node_modules/git-cliff-darwin-arm64"
+  * @example "x/xx/node_modules/envio-darwin-arm64"
   */
 function getExePath() {
   const arch = process.arch;
@@ -21,14 +21,14 @@ function getExePath() {
 
   try {
     // Since the bin will be located inside `node_modules`, we can simply call require.resolve
-    return require.resolve(`git-cliff-${os}-${arch}/bin/git-cliff${extension}`)
+    return require.resolve(`envio-${os}-${arch}/bin/envio${extension}`)
   } catch (e) {
-    throw new Error(`Couldn't find git-cliff binary inside node_modules for ${os}-${arch}`)
+    throw new Error(`Couldn't find envio binary inside node_modules for ${os}-${arch}`)
   }
 }
 
 /**
-  * Runs `git-cliff` with args using nodejs spawn
+  * Runs `envio` with args using nodejs spawn
   */
 function runGitCliff() {
   const args = process.argv.slice(2)
