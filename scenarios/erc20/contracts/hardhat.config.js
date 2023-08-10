b1fee03ad27cd4fb352f98f55950b7a95eb12cf1
Jason <jason@envio.dev>
2023-08-10 12:48:11 +0200

    Deploy and run some test transactions

--- scenarios/erc20/contracts/hardhat.config.js
+++ scenarios/erc20/contracts/hardhat.config.js
@@ -3,6 +3,25 @@ require("@nomiclabs/hardhat-ethers");
 require("hardhat-abi-exporter");
 require("./tasks");
 
+function getMnemonic(networkName) {
+  if (networkName) {
+    const mnemonic = process.env['MNEMONIC_' + networkName.toUpperCase()];
+    if (mnemonic && mnemonic !== '') {
+      return mnemonic;
+    }
+  }
+
+  const mnemonic = process.env.MNEMONIC;
+  if (!mnemonic || mnemonic === '') {
+    return 'test test test test test test test test test test test junk';
+  }
+  return mnemonic;
+}
+
+function accounts(networkName) {
+  return { mnemonic: getMnemonic(networkName) };
+}
+
 module.exports = {
   solidity: {
     version: "0.8.19",
@@ -11,6 +30,18 @@ module.exports = {
   networks: {
     ganache: {
       url: "http://localhost:8545",
+    },
+    mumbai: {
+      // url: "https://rpc.goerli.eth.gateway.fm",
+      // url: "https://rpc.ankr.com/eth_goerli",
+      url: "https://endpoints.omniatech.io/v1/eth/goerli/public",
+      accounts: accounts('goerli'), // To specify this via mnemonic use `MNEMONIC_GOERLI="your mnemonic"`
+    },
+    goerli: {
+      // url: "https://rpc.goerli.eth.gateway.fm",
+      // url: "https://rpc.ankr.com/eth_goerli",
+      url: "https://endpoints.omniatech.io/v1/eth/goerli/public",
+      accounts: accounts('goerli'), // To specify this via mnemonic use `MNEMONIC_GOERLI="your mnemonic"`
     },
   },
   abiExporter: {
