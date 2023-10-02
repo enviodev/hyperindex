be1b7a73bce8479f9e9feede241ec99e3b102e90
denhampreen <denhampreen@gmail.com>
2023-10-02 17:26:49 +0200

    refactor: unecesary type casting in js

--- codegenerator/cli/templates/static/erc20_template/javascript/src/EventHandlers.js
+++ codegenerator/cli/templates/static/erc20_template/javascript/src/EventHandlers.js
@@ -9,7 +9,7 @@ ERC20Contract.Approval.handler((event, context) => {
   //  getting the owner accountEntity
   let ownerAccount = context.account.get(event.params.owner);
 
-  if (ownerAccount == undefined) {
+  if (ownerAccount === undefined) {
     // create the account
     // This is an unlikely scenario, but it is possible
     let accountObject = {
@@ -40,12 +40,12 @@ ERC20Contract.Transfer.loader((event, context) => {
 ERC20Contract.Transfer.handler((event, context) => {
   let senderAccount = context.account.get(event.params.from);
 
-  if (senderAccount == undefined) {
+  if (senderAccount === undefined) {
     // create the account
     // This is likely only ever going to be the zero address in the case of the first mint
     let accountObject = {
       id: event.params.from,
-      balance: BigInt(0 - Number(event.params.value)),
+      balance: BigInt(0) - event.params.value,
     };
 
     context.account.set(accountObject);
@@ -53,9 +53,7 @@ ERC20Contract.Transfer.handler((event, context) => {
     // subtract the balance from the existing users balance
     let accountObject = {
       id: senderAccount.id,
-      balance: BigInt(
-        Number(senderAccount.balance) - Number(event.params.value)
-      ),
+      balance: senderAccount.balance - event.params.value,
     };
     context.account.set(accountObject);
   }
@@ -63,20 +61,18 @@ ERC20Contract.Transfer.handler((event, context) => {
   // getting the sender accountEntity
   let receiverAccount = context.account.get(event.params.to);
 
-  if (receiverAccount == undefined) {
+  if (receiverAccount === undefined) {
     // create new account
     let accountObject = {
       id: event.params.to,
-      balance: BigInt(event.params.value),
+      balance: event.params.value,
     };
     context.account.set(accountObject);
   } else {
     // update existing account
     let accountObject = {
       id: receiverAccount.id,
-      balance: BigInt(
-        Number(receiverAccount.balance) + Number(event.params.value)
-      ),
+      balance: receiverAccount.balance + event.params.value,
     };
 
     context.account.set(accountObject);
