15838037fa349659a8316bbfd2324912931a2f7a
Jason <jason@envio.dev>
2023-10-12 17:58:36 +0200

    Fix by using lower case characters

--- codegenerator/integration_tests/tests/runAll.sh
+++ codegenerator/integration_tests/tests/runAll.sh
@@ -4,38 +4,14 @@ set -e # Exit on error
 
 cargo run
 
-# # NOTE: this test must be run from the root `integration_tests` folder via `./tests/runAll.sh`
-#
-# export TEMPLATE="Erc20"
-# LANGUAGE="Javascript" ./tests/runSingle.sh
-# LANGUAGE="Typescript" ./tests/runSingle.sh
-# LANGUAGE="Rescript" ./tests/runSingle.sh
-#
-# export TEMPLATE="Greeter"
-# LANGUAGE="Javascript" ./tests/runSingle.sh
-# LANGUAGE="Typescript" ./tests/runSingle.sh
-# LANGUAGE="Rescript" ./tests/runSingle.sh
-
-run_single() {
-	echo "Running for $TEMPLATE in $LANGUAGE"
-	LANGUAGE=$1 ./tests/runSingle.sh &
-	pid=$!
-	echo "Started with PID $pid"
-	wait $pid
-	echo "Completed for $TEMPLATE in $LANGUAGE"
-}
+# NOTE: this test must be run from the root `integration_tests` folder via `./tests/runAll.sh`
 
 export TEMPLATE="Erc20"
-run_single "Javascript"
-sleep 5 # This sleep 'should' be completely unnecessary - but for some reason makes the tests less flaky :/ Really not sure why the tests sometimes fail
-run_single "Typescript"
-sleep 5 # This sleep 'should' be completely unnecessary - but for some reason makes the tests less flaky :/ Really not sure why the tests sometimes fail
-run_single "Rescript"
-sleep 5 # This sleep 'should' be completely unnecessary - but for some reason makes the tests less flaky :/ Really not sure why the tests sometimes fail
+LANGUAGE="Javascript" ./tests/runSingle.sh
+LANGUAGE="Typescript" ./tests/runSingle.sh
+LANGUAGE="Rescript" ./tests/runSingle.sh
 
 export TEMPLATE="Greeter"
-run_single "Javascript"
-sleep 5 # This sleep 'should' be completely unnecessary - but for some reason makes the tests less flaky :/ Really not sure why the tests sometimes fail
-run_single "Typescript"
-sleep 5 # This sleep 'should' be completely unnecessary - but for some reason makes the tests less flaky :/ Really not sure why the tests sometimes fail
-run_single "Rescript"
+LANGUAGE="Javascript" ./tests/runSingle.sh
+LANGUAGE="Typescript" ./tests/runSingle.sh
+LANGUAGE="Rescript" ./tests/runSingle.sh
