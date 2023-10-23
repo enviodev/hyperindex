7f9e1a5855a75fc97c5a7a6bfaf8feb091ff8aaa
Jono Prest <jjprest@gmail.com>
2023-10-23 14:18:43 +0200

    Renamed to uppercase

--- codegenerator/integration_tests/tests/Erc20.js
+++ codegenerator/integration_tests/tests/Erc20.js
@@ -3,11 +3,13 @@ const { exit } = require("process");
 let maxRetries = 200;
 
 let shouldExitOnFailure = false; // This flag is set to true once all setup has completed and test is being performed.
+let maxRetries = 120;
 
 const pollGraphQL = async () => {
   const rawEventsQuery = `
     query {
       raw_events_by_pk(chain_id: 1, event_id: "712791818308") {
+      raw_events_by_pk(chain_id: 5, event_id: "622184760610") {
         event_type
         log_index
         src_address
@@ -20,12 +22,8 @@ const pollGraphQL = async () => {
 
   const accountEntityQuery = `
     {
-      Account_by_pk(id: "0x26921A182Cf9D6F33730D7F37E1a86fd430863Af") {
-        approvals(order_by: {event_id: asc}, limit: 2) {
-          amount
-          owner
-          spender
-        }
+      account_by_pk(id: "0x0000000000000000000000000000000000000000") {
+        approval
         balance
         id
       }
@@ -67,37 +65,25 @@ const pollGraphQL = async () => {
         console.error(errors);
       }
     } catch (err) {
-      if (!shouldExitOnFailure) {
-        console.log("[will retry] Could not request data from Hasura due to error: ", err);
-        console.log("Hasura not yet started, retrying in 2s");
-      } else {
-        console.error(err);
-        process.exit(1);
-      }
+      console.log("Could not request data from Hasura due to error: ", err);
+      console.log("Hasura not yet started, retrying in 1s");
     }
-    setTimeout(() => { if (!shouldExitOnFailure) fetchQuery(query, callback) }, 2000);
+    setTimeout(() => fetchQuery(query, callback), 1000);
   };
-
-  console.log("Starting running test")
 
   // TODO: make this use promises rather than callbacks.
   fetchQuery(rawEventsQuery, (data) => {
     assert(
-      data.raw_events_by_pk.event_type === "ERC20Contract_ApprovalEvent",
-      "event_type should be an ApprovalEvent"
+      data.raw_events_by_pk.event_type === "ERC20Contract_TransferEvent",
+      "event_type should be TransferEvent"
     );
     console.log("First test passed, running the second one.");
 
-
     // Run the second test
-    fetchQuery(accountEntityQuery, ({ Account_by_pk: account }) => {
+    fetchQuery(accountEntityQuery, ({ account_by_pk: account }) => {
       assert(!!account, "account should not be null or undefined");
-      shouldExitOnFailure = true;
-      assert(account.balance > 311465476000000000000, "balance should be == 70"); // NOTE the balance shouldn't change since we own this erc20 token.
-      assert(account.approvals.length > 0, "There should be at least one approval");
-      assert(account.approvals[0].amount == 79228162514264337593543950335n, "The first approval amount should be 50");
-      assert(account.approvals[0].owner == account.id, "The first approval owner should be the account id");
-      assert(account.approvals[0].spender == "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D", "The first approval spender should be correct (from uniswap)");
+      assert(account.balance <= -103, "balance should be <= -103");
+      assert(account.approval == 0, "approval should be = 0");
       console.log("Second test passed.");
     });
   });
@@ -105,3 +91,5 @@ const pollGraphQL = async () => {
 
 pollGraphQL();
 
+// After all async tasks are done
+process.exit(0);
