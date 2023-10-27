5b82e57d5e652d7e1458aef7b8bc3759c168acbb
Denham Preen <denhampreen@gmail.com>
2023-10-27 21:16:13 +0200

    Revert "fix: hosted service deployments"
    This reverts commit be58d4ad7aa3d7f01e21002264ff2f7c3ec14f73.

--- codegenerator/cli/src/hbs_templating/init_templates.rs
+++ codegenerator/cli/src/hbs_templating/init_templates.rs
@@ -22,7 +22,7 @@ impl InitTemplates {
             crate_version.to_string()
         } else {
             //Else install the latest version from npm so as not to break dev environments
-            "0.0.22".to_string()
+            "latest".to_string()
         };
 
         let template = InitTemplates {
