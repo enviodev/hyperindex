273e1e07199b561a3ed8c6432dc302694bba83b5
Jason <jason@envio.dev>
2023-11-14 10:50:50 +0200

    Upgrade to using the cluster for endpoints

--- codegenerator/cli/src/config_parsing/hypersync_endpoints.rs
+++ codegenerator/cli/src/config_parsing/hypersync_endpoints.rs
@@ -36,16 +36,16 @@ pub fn network_to_eth_archive_url(network: &EthArchiveNetwork) -> String {
 
 pub fn network_to_skar_url(network: &SkarNetwork) -> String {
     match network {
-        SkarNetwork::EthereumMainnet => "http://eth.hypersync.bigdevenergy.link:1100".to_string(),
-        SkarNetwork::Polygon => "http://polygon.hypersync.bigdevenergy.link:1101".to_string(),
-        SkarNetwork::Gnosis => "http://gnosis.hypersync.bigdevenergy.link:1102".to_string(),
-        SkarNetwork::Bsc => "http://bsc.hypersync.bigdevenergy.link:1103".to_string(),
-        SkarNetwork::Goerli => "http://goerli.hypersync.bigdevenergy.link:1104".to_string(),
-        SkarNetwork::Optimism => "http://optimism.hypersync.bigdevenergy.link:1105".to_string(),
-        SkarNetwork::ArbitrumOne => "http://arbitrum.hypersync.bigdevenergy.link:1106".to_string(),
-        SkarNetwork::Linea => "http://linea.hypersync.bigdevenergy.link:1107".to_string(),
-        SkarNetwork::Sepolia => "http://sepolia.hypersync.bigdevenergy.link:1108".to_string(),
-        SkarNetwork::Base => "http://base.hypersync.bigdevenergy.link:1109".to_string(),
+        SkarNetwork::EthereumMainnet => "https://eth.hypersync.bigdevenergy.link:1100".to_string(),
+        SkarNetwork::Polygon => "https://polygon.hypersync.bigdevenergy.link:1101".to_string(),
+        SkarNetwork::Gnosis => "https://gnosis.hypersync.bigdevenergy.link:1102".to_string(),
+        SkarNetwork::Bsc => "https://bsc.hypersync.bigdevenergy.link:1103".to_string(),
+        SkarNetwork::Goerli => "https://goerli.hypersync.bigdevenergy.link:1104".to_string(),
+        SkarNetwork::Optimism => "https://optimism.hypersync.bigdevenergy.link:1105".to_string(),
+        SkarNetwork::ArbitrumOne => "https://arbitrum.hypersync.bigdevenergy.link:1106".to_string(),
+        SkarNetwork::Linea => "https://linea.hypersync.bigdevenergy.link:1107".to_string(),
+        SkarNetwork::Sepolia => "https://sepolia.hypersync.bigdevenergy.link:1108".to_string(),
+        SkarNetwork::Base => "https://base.hypersync.bigdevenergy.link:1109".to_string(),
     }
 }
 
