b37fe708846c900d3db35f1f4ac352fe4536679e
Jono Prest <jjprest@gmail.com>
2023-11-27 17:43:12 +0200

    Cleanup Greeter handlers and fix bugs

--- codegenerator/cli/templates/static/greeter_template/javascript/src/EventHandlers.js
+++ codegenerator/cli/templates/static/greeter_template/javascript/src/EventHandlers.js
@@ -1,45 +1,90 @@
 let { GreeterContract } = require("../generated/src/Handlers.bs.js");
 
+/**
+Registers a loader that loads any values from your database that your
+NewGreeting event handler might need on the Greeter contract.
+*/
 GreeterContract.NewGreeting.loader((event, context) => {
-  context.Greeting.load(event.params.user);
+  //The id for the "Greeting" entity derived from params of the NewGreeting event
+  const userId = event.params.user;
+  //Try load in in a "Greeting" entity with id of the user param on the
+  //NewGreeting event
+  context.Greeting.load(userId);
 });
 
+/**
+Registers a handler that handles any values from the
+NewGreeting event on the Greeter contract and index these values into
+the DB.
+*/
 GreeterContract.NewGreeting.handler((event, context) => {
-  let user = event.params.user;
-  let latestGreeting = event.params.greeting;
-  let numberOfGreetings = event.params.numberOfGreetings;
+  //The id for the "Greeting" entity
+  const userId = event.params.user;
+  //The greeting string that was added.
+  const latestGreeting = event.params.greeting;
 
-  let existingGreeter = context.Greeting.get(event.params.user);
+  //The optional greeting entity that may exist already at "userId"
+  //This value would be undefined in the case that it was not loaded in the
+  //loader function above OR in the case where it never existed in the db
+  const currentGreetingEntity = context.Greeting.get(userId);
 
-  if (existingGreeter !== undefined) {
-    context.Greeting.set({
-      id: user,
-      latestGreeting: latestGreeting,
-      numberOfGreetings: existingGreeter.numberOfGreetings + 1,
-      greetings: [...existingGreeter.greetings, latestGreeting],
-    });
-  } else {
-    context.Greeting.set({
-      id: user,
-      latestGreeting: latestGreeting,
+  //Construct the greetingEntity that is to be set in the DB
+  const greetingEntity = currentGreetingEntity
+    ? //In the case there is an existing "Greeting" entity, update its
+    //latestGreeting value, increment the numberOfGreetings and append latestGreeting
+    //to the array of greetings
+    {
+      id: userId,
+      latestGreeting,
+      numberOfGreetings: currentGreetingEntity.numberOfGreetings + 1,
+      greetings: [...currentGreetingEntity.greetings, latestGreeting],
+    }
+    : //In the case where there is no Greeting entity at this id. Construct a new one with
+    //the current latest greeting, an initial number of greetings as "1" and an initial list
+    //of greetings with only the latest greeting.
+    {
+      id: userId,
+      latestGreeting,
       numberOfGreetings: 1,
       greetings: [latestGreeting],
-    });
-  }
+    };
+
+  //Set the greeting entity in the DB with the constructed values
+  context.Greeting.set(greetingEntity);
 });
 
+/**
+Registers a loader that loads any values from your database that your
+ClearGreeting event handler might need on the Greeter contract.
+*/
 GreeterContract.ClearGreeting.loader((event, context) => {
-  context.Greeting.load(event.params.user);
+  //The id for the "Greeting" entity derived from params of the ClearGreeting event
+  const userId = event.params.user;
+  //Try load in in a "Greeting" entity with id of the user param on the
+  //ClearGreeting event
+  context.Greeting.load(userId);
 });
 
+/**
+Registers a handler that handles any values from the
+ClearGreeting event on the Greeter contract and index these values into
+the DB.
+*/
 GreeterContract.ClearGreeting.handler((event, context) => {
-  let existingGreeter = context.Greeting.get(event.params.user);
-  if (existingGreeter !== undefined) {
+  //The id for the "Greeting" entity derived from params of the ClearGreeting event
+  const userId = event.params.user;
+  //The optional greeting entity that may exist already at "userId"
+  //This value would be "undefined" in the case that it was not loaded in the
+  //loader function above OR in the case where it never existed in the db
+  const currentGreetingEntity = context.Greeting.get(userId);
+
+  if (currentGreetingEntity) {
+    //Only make any changes in the case that there is an existing Greeting
+    //Simply clear the latestGreeting by setting it to "" (empty string)
+    //and keep all the rest of the data the same
     context.Greeting.set({
-      id: user,
+      ...currentGreetingEntity,
       latestGreeting: "",
-      numberOfGreetings: existingGreeter.numberOfGreetings + 1,
-      greetings: existingGreeter.greetings,
     });
   }
 });
--- codegenerator/cli/templates/static/greeter_template/typescript/src/EventHandlers.ts
+++ codegenerator/cli/templates/static/greeter_template/typescript/src/EventHandlers.ts
@@ -7,48 +7,91 @@ import {
 
 import { GreetingEntity } from "../generated/src/Types.gen";
 
+/**
+Registers a loader that loads any values from your database that your
+NewGreeting event handler might need on the Greeter contract.
+*/
 GreeterContract_NewGreeting_loader(({ event, context }) => {
-  context.Greeting.load(event.params.user.toString());
+  //The id for the "Greeting" entity derived from params of the NewGreeting event
+  const userId = event.params.user;
+  //Try load in in a "Greeting" entity with id of the user param on the
+  //NewGreeting event
+  context.Greeting.load(userId);
 });
 
+/**
+Registers a handler that handles any values from the
+NewGreeting event on the Greeter contract and index these values into
+the DB.
+*/
 GreeterContract_NewGreeting_handler(({ event, context }) => {
-  let currentGreeter = context.Greeting.get(event.params.user);
+  //The id for the "Greeting" entity
+  const userId = event.params.user;
+  //The greeting string that was added.
+  const latestGreeting = event.params.greeting;
 
-  if (currentGreeter != null) {
-    let greetingObject: GreetingEntity = {
-      id: event.params.user.toString(),
-      latestGreeting: event.params.greeting,
-      numberOfGreetings: currentGreeter.numberOfGreetings + 1,
-      greetings: [...currentGreeter.greetings, event.params.greeting],
-    };
+  //The optional greeting entity that may exist already at "userId"
+  //This value would be undefined in the case that it was not loaded in the
+  //loader function above OR in the case where it never existed in the db
+  const currentGreetingEntity = context.Greeting.get(userId);
 
-    context.Greeting.set(greetingObject);
-  } else {
-    let greetingObject: GreetingEntity = {
-      id: event.params.user.toString(),
-      latestGreeting: event.params.greeting,
+  //Construct the greetingEntity that is to be set in the DB
+  const greetingEntity: GreetingEntity = currentGreetingEntity
+    ? //In the case there is an existing "Greeting" entity, update its
+    //latestGreeting value, increment the numberOfGreetings and append latestGreeting
+    //to the array of greetings
+    {
+      id: userId,
+      latestGreeting,
+      numberOfGreetings: currentGreetingEntity.numberOfGreetings + 1,
+      greetings: [...currentGreetingEntity.greetings, latestGreeting],
+    }
+    : //In the case where there is no Greeting entity at this id. Construct a new one with
+    //the current latest greeting, an initial number of greetings as "1" and an initial list
+    //of greetings with only the latest greeting.
+    {
+      id: userId,
+      latestGreeting,
       numberOfGreetings: 1,
-      greetings: [event.params.greeting],
+      greetings: [latestGreeting],
     };
-    context.Greeting.set(greetingObject);
-  }
+
+  //Set the greeting entity in the DB with the constructed values
+  context.Greeting.set(greetingEntity);
 });
 
+/**
+Registers a loader that loads any values from your database that your
+ClearGreeting event handler might need on the Greeter contract.
+*/
 GreeterContract_ClearGreeting_loader(({ event, context }) => {
-  context.Greeting.load(event.params.user.toString());
+  //The id for the "Greeting" entity derived from params of the ClearGreeting event
+  const userId = event.params.user;
+  //Try load in in a "Greeting" entity with id of the user param on the
+  //ClearGreeting event
+  context.Greeting.load(userId);
 });
 
+/**
+Registers a handler that handles any values from the
+ClearGreeting event on the Greeter contract and index these values into
+the DB.
+*/
 GreeterContract_ClearGreeting_handler(({ event, context }) => {
-  let currentGreeter = context.Greeting.get(event.params.user);
+  //The id for the "Greeting" entity derived from params of the ClearGreeting event
+  const userId = event.params.user;
+  //The optional greeting entity that may exist already at "userId"
+  //This value would be "undefined" in the case that it was not loaded in the
+  //loader function above OR in the case where it never existed in the db
+  const currentGreetingEntity = context.Greeting.get(userId);
 
-  if (currentGreeter != null) {
-    let greetingObject: GreetingEntity = {
-      id: event.params.user.toString(),
+  if (currentGreetingEntity) {
+    //Only make any changes in the case that there is an existing Greeting
+    //Simply clear the latestGreeting by setting it to "" (empty string)
+    //and keep all the rest of the data the same
+    context.Greeting.set({
+      ...currentGreetingEntity,
       latestGreeting: "",
-      numberOfGreetings: currentGreeter.numberOfGreetings,
-      greetings: currentGreeter.greetings,
-    };
-
-    context.Greeting.set(greetingObject);
+    });
   }
 });
