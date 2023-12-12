b1d74be250a6f98db9f2cf63edd293a1edc06eb2
Jono Prest <jjprest@gmail.com>
2023-12-12 15:33:58 +0200

    Add js test

--- codegenerator/cli/templates/static/greeter_template/javascript/test/test.js
+++ codegenerator/cli/templates/static/greeter_template/javascript/test/test.js
@@ -1,30 +1,30 @@
-const { expect } = require("chai");
+const assert = require("assert");
 const { MockDb, Greeter } = require("../generated/src/TestHelpers.bs");
 const { Addresses } = require("../generated/src/bindings/Ethers.bs");
 
 describe("Greeter template tests", () => {
   it("A NewGreeting event creates a Greeting entity", () => {
     // Initializing the mock database
-    let mockDbInitial = MockDb.createMockDb();
+    const mockDbInitial = MockDb.createMockDb();
 
     // Initializing values for mock event
-    let userAddress = Addresses.defaultAddress;
-    let greeting = "Hi there";
+    const userAddress = Addresses.defaultAddress;
+    const greeting = "Hi there";
 
     // Creating a mock event
-    let mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
+    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
       greeting: greeting,
       user: userAddress,
     });
 
     // Processing the mock event on the mock database
-    let updatedMockDb = Greeter.NewGreeting.processEvent({
+    const updatedMockDb = Greeter.NewGreeting.processEvent({
       event: mockNewGreetingEvent,
       mockDb: mockDbInitial,
     });
 
     // Expected entity that should be created
-    let expectedGreetingEntity = {
+    const expectedGreetingEntity = {
       id: userAddress,
       latestGreeting: greeting,
       numberOfGreetings: 1,
@@ -32,93 +32,89 @@ describe("Greeter template tests", () => {
     };
 
     // Getting the entity from the mock database
-    let dbEntity = updatedMockDb.entities.Greeting.get(userAddress);
+    const dbEntity = updatedMockDb.entities.Greeting.get(userAddress);
 
     // Asserting that the entity in the mock database is the same as the expected entity
-    expect(expectedGreetingEntity).to.deep.equal(dbEntity);
+    assert.deepEqual(expectedGreetingEntity, dbEntity);
   });
 
   it("2 Greetings from the same users results in that user having a greeter count of 2", () => {
     // Initializing the mock database
-    let mockDbInitial = MockDb.createMockDb();
-
+    const mockDbInitial = MockDb.createMockDb();
     // Initializing values for mock event
-    let userAddress = Addresses.defaultAddress;
-    let greeting = "Hi there";
-    let greetingAgain = "Oh hello again";
+    const userAddress = Addresses.defaultAddress;
+    const greeting = "Hi there";
+    const greetingAgain = "Oh hello again";
 
     // Creating a mock event
-    let mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
+    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
       greeting: greeting,
       user: userAddress,
     });
 
     // Creating a mock event
-    let mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
+    const mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
       greeting: greetingAgain,
       user: userAddress,
     });
 
     // Processing the mock event on the mock database
-    let updatedMockDb = Greeter.NewGreeting.processEvent({
+    const updatedMockDb = Greeter.NewGreeting.processEvent({
       event: mockNewGreetingEvent,
       mockDb: mockDbInitial,
     });
 
     // Processing the mock event on the updated mock database
-    let updatedMockDb2 = Greeter.NewGreeting.processEvent({
+    const updatedMockDb2 = Greeter.NewGreeting.processEvent({
       event: mockNewGreetingEvent2,
       mockDb: updatedMockDb,
     });
 
-    let expectedGreetingCount = 2;
-
     // Getting the entity from the mock database
-    let dbEntity = updatedMockDb2.entities.Greeting.get(userAddress);
+    const dbEntity = updatedMockDb2.entities.Greeting.get(userAddress);
 
     // Asserting that the field value of the entity in the mock database is the same as the expected field value
-    expect(dbEntity?.numberOfGreetings).to.deep.equal(expectedGreetingCount);
+    assert.equal(2, dbEntity?.numberOfGreetings);
   });
 
   it("2 Greetings from the same users results in the latest greeting being the greeting from the second event", () => {
     // Initializing the mock database
-    let mockDbInitial = MockDb.createMockDb();
-
+    const mockDbInitial = MockDb.createMockDb();
     // Initializing values for mock event
-    let userAddress = Addresses.defaultAddress;
-    let greeting = "Hi there";
-    let greetingAgain = "Oh hello again";
+    const userAddress = Addresses.defaultAddress;
+    const greeting = "Hi there";
+    const greetingAgain = "Oh hello again";
 
     // Creating a mock event
-    let mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
+    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
       greeting: greeting,
       user: userAddress,
     });
 
     // Creating a mock event
-    let mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
+    const mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
       greeting: greetingAgain,
       user: userAddress,
     });
 
     // Processing the mock event on the mock database
-    let updatedMockDb = Greeter.NewGreeting.processEvent({
+    const updatedMockDb = Greeter.NewGreeting.processEvent({
       event: mockNewGreetingEvent,
       mockDb: mockDbInitial,
     });
 
     // Processing the mock event on the updated mock database
-    let updatedMockDb2 = Greeter.NewGreeting.processEvent({
+    const updatedMockDb2 = Greeter.NewGreeting.processEvent({
       event: mockNewGreetingEvent2,
       mockDb: updatedMockDb,
     });
 
-    let expectedGreeting = greetingAgain;
-
     // Getting the entity from the mock database
-    let dbEntity = updatedMockDb2.entities.Greeting.get(userAddress);
+    const dbEntity = updatedMockDb2.entities.Greeting.get(userAddress);
+
+    const expectedGreeting = greetingAgain;
 
     // Asserting that the field value of the entity in the mock database is the same as the expected field value
-    expect(dbEntity?.latestGreeting).to.deep.equal(expectedGreeting);
+    assert.equal(expectedGreeting, dbEntity?.latestGreeting);
   });
 });
