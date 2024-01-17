48118862edabecc7d9487551747a9d1e67a9e823
Jono Prest <jjprest@gmail.com>
2024-01-17 13:09:46 +0200

    Fix tests with confirmed threshold in config

--- scenarios/test_codegen/test/integration-raw-events-test.ts
+++ scenarios/test_codegen/test/integration-raw-events-test.ts
@@ -37,6 +37,7 @@ describe("Raw Events Integration", () => {
     const { provider } = hre.ethers;
 
     return {
+      confirmedBlockThreshold: 200,
       syncSource: {
         TAG: /* Rpc */ 0,
         _0: {
@@ -70,7 +71,7 @@ describe("Raw Events Integration", () => {
     };
   };
 
-  before(async function() {
+  before(async function () {
     this.timeout(30 * 1000);
     // setLogLevel("trace");
 
@@ -141,12 +142,12 @@ describe("Raw Events Integration", () => {
     await runMigrationsNoLogs();
   });
 
-  it("RawEvents table contains rows after indexer runs", async function() {
+  it("RawEvents table contains rows after indexer runs", async function () {
     let rawEventsRows = await sql`SELECT * FROM public.raw_events`;
     expect(rawEventsRows.count).to.be.gt(0);
   });
 
-  it("should ensure Entites are created correctly", async function() {
+  it("should ensure Entites are created correctly", async function () {
     let rowsNftCollection = await sql`SELECT * FROM public."NftCollection"`;
     expect(rowsNftCollection.count).to.be.gt(0);
     let rowsUsers = await sql`SELECT * FROM public."User"`;
@@ -155,7 +156,7 @@ describe("Raw Events Integration", () => {
     expect(rowsToken.count).to.be.gt(0);
   });
 
-  it("should have 1 row in the dynamic_contract_registry table", async function() {
+  it("should have 1 row in the dynamic_contract_registry table", async function () {
     let rowsDCR = await sql`SELECT * FROM public.dynamic_contract_registry`;
     console.log(rowsDCR);
     expect(rowsDCR.count).to.be.eq(1);
@@ -188,13 +189,13 @@ describe("Raw Events Integration", () => {
     expect(afterRawEventsRows.count).to.be.gt(beforeRawEventsRows.count);
   });
 
-  it("RawEvents table does contains rows after migration keeping raw events table", async function() {
+  it("RawEvents table does contains rows after migration keeping raw events table", async function () {
     await runDownMigrations(false, false);
     let rawEventsRows = await sql`SELECT * FROM public.raw_events`;
     expect(rawEventsRows.count).to.be.gt(0);
   });
 
-  it("RawEvents table does not exist after migration dropping raw events table", async function() {
+  it("RawEvents table does not exist after migration dropping raw events table", async function () {
     await runDownMigrations(false, true);
     let rawEventsRows = await sql`
         SELECT EXISTS (
