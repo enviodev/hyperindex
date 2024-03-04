bdb86e3c3937de9d8780bf5dd3a97e966445696c
Jono Prest <jjprest@gmail.com>
2024-03-04 15:27:54 +0200

    Make viem the default decoder until address checksum is added to hypersync client

--- codegenerator/cli/src/config_parsing/system_config.rs
+++ codegenerator/cli/src/config_parsing/system_config.rs
@@ -260,7 +260,7 @@ impl SystemConfig {
             event_decoder: human_cfg
                 .event_decoder
                 .clone()
-                .unwrap_or(EventDecoder::HypersyncClient),
+                .unwrap_or(EventDecoder::Viem),
             schema,
         })
     }
