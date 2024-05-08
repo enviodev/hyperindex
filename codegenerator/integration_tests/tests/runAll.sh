ce7df50cf02bf6122f0a2fc307889f8d1a09167a
Mike <mj.young114@gmail.com>
2024-05-08 14:11:15 +0200

    undo comment

--- codegenerator/integration_tests/tests/runAll.sh
+++ codegenerator/integration_tests/tests/runAll.sh
@@ -2,7 +2,7 @@
 
 set -e # Exit on error
 
-# cargo run #calls the function run_all_init_combinations in main.rs to create all templates
+cargo run #calls the function run_all_init_combinations in main.rs to create all templates
 
 # NOTE: this test must be run from the root `integration_tests` folder via `./tests/runAll.sh`
 
