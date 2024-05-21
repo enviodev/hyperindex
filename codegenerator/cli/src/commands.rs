24f80daa7563074f5328fd4756022e2b7152ba3e
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-05-21 19:36:07 +0400

    Add --yes arg for npx just in case

--- codegenerator/cli/src/commands.rs
+++ codegenerator/cli/src/commands.rs
@@ -151,7 +151,7 @@ pub mod codegen {
         project_paths: &ParsedProjectPaths,
     ) -> anyhow::Result<()> {
         let package = format!("envio@{}", envio_version);
-        let args = vec![package.as_str(), "codegen"];
+        let args = vec![package.as_str(), "codegen", "--yes"];
         execute_command("npx", args, &project_paths.project_root)
             .await
             .context("Failed to execute codegen")?;
