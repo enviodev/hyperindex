6edd0ee79a4d4c66ca02295e5e607198102b6a48
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-06-19 10:38:44 +0400

    [V1] Remove EventSummary from init template (#34)
    * Remove EventSummary from init template
    
    * Revert the code which I shouldn't remove

--- codegenerator/cli/src/config_parsing/human_config.rs
+++ codegenerator/cli/src/config_parsing/human_config.rs
@@ -32,6 +32,7 @@ pub mod evm {
         pub name: String,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub description: Option<String>,
+        #[serde(skip_serializing_if = "Option::is_none")]
         pub ecosystem: Option<EcosystemTag>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub schema: Option<String>,
