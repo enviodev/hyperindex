7f3845e0dcdb727a276142f0820a3b0a3d887eb0
Jono Prest <jjprest@gmail.com>
2024-07-11 10:27:20 +0200

    Remove transaction fields from test query

--- codegenerator/integration_tests/tests/evm_Greeter.js
+++ codegenerator/integration_tests/tests/evm_Greeter.js
@@ -1,7 +1,10 @@
 const assert = require("assert");
-const { fetchQueryWithTestCallback } = require("./graphqlFetchWithTestCallback");
+const {
+  fetchQueryWithTestCallback,
+} = require("./graphqlFetchWithTestCallback");
 
-const maxRetryFailureMessage = "Max retries reached - either increase the timeout (maxRetries) or check for other bugs."
+const maxRetryFailureMessage =
+  "Max retries reached - either increase the timeout (maxRetries) or check for other bugs.";
 
 const pollGraphQL = async () => {
   const rawEventsQuery = `
@@ -10,8 +13,6 @@ const pollGraphQL = async () => {
         event_type
         log_index
         src_address
-        transaction_hash
-        transaction_index
         block_number
       }
     }
@@ -34,34 +35,41 @@ const pollGraphQL = async () => {
     try {
       assert(
         data.raw_events_by_pk.event_type === "Greeter_NewGreeting",
-        "event_type should be Greeter_NewGreeting"
+        "event_type should be Greeter_NewGreeting",
       );
-      console.log("First greeter passed, running the second one for user entity");
+      console.log(
+        "First greeter passed, running the second one for user entity",
+      );
 
       // Run the second test
-      fetchQueryWithTestCallback(userEntityQuery, maxRetryFailureMessage, ({ User_by_pk: user }) => {
-        try {
-          assert(!!user, "greeting should not be null or undefined");
-          assert(
-            user.greetings.slice(0, 3).toString() === "gm,gn,gm paris",
-            "First 3 greetings should be 'gm,gn,gm paris'"
-          );
-          assert(user.numberOfGreetings >= 3, "numberOfGreetings should be >= 3");
-          console.log("Second test passed.");
-        }
-        catch (err) {
-          //gotta love javascript
-          err.shouldExitOnFailure = shouldExitOnFailure
-          throw err;
-        }
-      });
+      fetchQueryWithTestCallback(
+        userEntityQuery,
+        maxRetryFailureMessage,
+        ({ User_by_pk: user }) => {
+          try {
+            assert(!!user, "greeting should not be null or undefined");
+            assert(
+              user.greetings.slice(0, 3).toString() === "gm,gn,gm paris",
+              "First 3 greetings should be 'gm,gn,gm paris'",
+            );
+            assert(
+              user.numberOfGreetings >= 3,
+              "numberOfGreetings should be >= 3",
+            );
+            console.log("Second test passed.");
+          } catch (err) {
+            //gotta love javascript
+            err.shouldExitOnFailure = shouldExitOnFailure;
+            throw err;
+          }
+        },
+      );
     } catch (err) {
       //gotta love javascript
-      err.shouldExitOnFailure = shouldExitOnFailure
+      err.shouldExitOnFailure = shouldExitOnFailure;
       throw err;
     }
   });
-
 };
 
 pollGraphQL();
