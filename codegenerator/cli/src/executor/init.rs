4f640b04c2e60232c54ea5e43920068d031cbfb7
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-07-12 11:44:16 +0400

    Treat rc as valid envio version

--- codegenerator/cli/src/executor/init.rs
+++ codegenerator/cli/src/executor/init.rs
@@ -27,8 +27,8 @@ use std::path::PathBuf;
 //Returns false if there are any additional chars as this should imply
 //it is a dev release version or an unstable release
 fn is_valid_release_version_number(version: &str) -> bool {
-    let re_version_pattern =
-        Regex::new(r"^\d+\.\d+\.\d+$").expect("version regex pattern should be valid regex");
+    let re_version_pattern = Regex::new(r"^\d+\.\d+\.\d+(-rc\.\d+)?$")
+        .expect("version regex pattern should be valid regex");
     re_version_pattern.is_match(version)
 }
 
@@ -316,7 +316,7 @@ mod test {
 
     #[test]
     fn test_valid_version_numbers() {
-        let valid_version_numbers = vec!["0.0.0", "999.999.999", "0.0.1", "10.2.3"];
+        let valid_version_numbers = vec!["0.0.0", "999.999.999", "0.0.1", "10.2.3", "2.0.0-rc.1"];
 
         for vn in valid_version_numbers {
             assert!(super::is_valid_release_version_number(vn));
