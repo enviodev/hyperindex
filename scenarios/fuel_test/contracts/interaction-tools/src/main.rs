4530f8a22b3f8a87c9cb230618b9b7c14c1d9022
Denham Preen <denhampreen@gmail.com>
2024-10-22 14:28:02 +0900

    Revert "feat: ts fuel all events deployment scripts for e2e testing (#249)"
    This reverts commit aa5492f4074164e6a3b01a919f02d3d711a5457f.

--- scenarios/fuel_test/contracts/interaction-tools/src/main.rs
+++ scenarios/fuel_test/contracts/interaction-tools/src/main.rs
@@ -17,20 +17,16 @@ async fn main() -> Result<()> {
     // Load .env file
     dotenv().ok();
     let phrase = env::var("MNEMONIC").expect("MNEMONIC must be set in .env");
-    let provider_url = env::var("PROVIDER_URL").expect("PROVIDER must be set in .env");
-    let base_asset_address = env::var("BASE_ASSET_ADDRESS").expect("BASE_ASSET_ADDRESS must be set in .env");
-    
-    let provider = Provider::connect(provider_url).await.unwrap();
-
+    let provider = Provider::connect("testnet.fuel.network").await.unwrap();
     let wallet = WalletUnlocked::new_from_mnemonic_phrase(&phrase, Some(provider.clone())).unwrap();
     let base_asset_id =
-        AssetId::from_str(&base_asset_address)
+        AssetId::from_str("0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07")
             .unwrap();
     let balance = provider
         .get_asset_balance(wallet.address(), base_asset_id)
         .await?;
     if balance < 500000 {
-        println!("Wallet address {}, with balance {}, can use the faucet here for testnet: https://faucet-testnet.fuel.network/?address={}", wallet.address(), balance, wallet.address());
+        println!("Wallet address {}, with balance {}, can use the faucet here: https://faucet-testnet.fuel.network/?address={}", wallet.address(), balance, wallet.address());
         return Ok(());
     }
 
@@ -56,26 +52,25 @@ async fn main() -> Result<()> {
     println!("Logs in tx: 0x{}", r.tx_id.unwrap());
 
     // LiquidityPool - deposit and withdraw for testing Mint/Burn/Call/TransferOut receipts
-    let deposit_amount = 1;
+    let deposit_amount = 100;
     let call_params = CallParameters::default()
         .with_amount(deposit_amount)
         .with_asset_id(base_asset_id);
-    let deposit_action = contract_methods
+    let r = contract_methods
         .deposit(wallet.address().into())
         .call_params(call_params)?
         .with_variable_output_policy(VariableOutputPolicy::Exactly(1))
         .call()
         .await?;
-    println!("Deposited 100 coins to LP in tx: 0x{}", deposit_action.tx_id.unwrap());
-    
+    println!("Deposited 100 coins to LP in tx: 0x{}", r.tx_id.unwrap());
     let lp_asset_id = contract_id.asset_id(&Bits256::zeroed());
     let lp_token_balance = wallet.get_asset_balance(&lp_asset_id).await?;
-    let withdraw_call_params = CallParameters::default()
+    let call_params = CallParameters::default()
         .with_amount(lp_token_balance)
         .with_asset_id(lp_asset_id);
-    let withdraw_action = contract_methods
+    let r = contract_methods
         .withdraw(wallet.address().into())
-        .call_params(withdraw_call_params)?
+        .call_params(call_params)?
         .with_variable_output_policy(VariableOutputPolicy::Exactly(1))
         .with_contract_ids(&[greeter_contract_id.into()])
         .call()
@@ -83,7 +78,7 @@ async fn main() -> Result<()> {
     println!(
         "Withdrawn {} tokens from LP in tx: 0x{}",
         lp_token_balance,
-        withdraw_action.tx_id.unwrap()
+        r.tx_id.unwrap()
     );
 
     println!("Successfully finished mock interactions with the contract: 0x{contract_id}",);
--- scenarios/fuel_test/contracts/ts-interaction-tools/src/@types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-      MNEMONIC?: string;
-      PROVIDER_URL?: string;
-      BASE_ASSET_ADDRESS?: string;
-    }
-  }
-  
