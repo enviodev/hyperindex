922c892cfe2a9489c061dee4b7e27d18919b5ba1
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-11-01 16:14:49 +0400

    Update local-bin

--- codegenerator/cli/npm/envio/local-bin.mjs
+++ codegenerator/cli/npm/envio/local-bin.mjs
@@ -4,21 +4,69 @@
 
 import { spawnSync } from "child_process";
 import path from "path";
-import { fileURLToPath } from "url";
 
 /**
  * Runs `envio` with args using nodejs spawn
  */
 function runLocalEnvio() {
   const args = process.argv.slice(2);
-  const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-  const __dirname = path.dirname(__filename); // get the name of the directory
+
+  const caller = process.argv[1];
+  if (!caller.endsWith("/node_modules/envio/local-bin.mjs")) {
+    throw new Error(
+      "Unexpected call to local envio package. Either use cargo or use the local package via npm scripts."
+    );
+  }
+
+  let root = path.join(caller, "../../..");
+
+  const pnpmListResult = spawnSync("pnpm", ["list", "envio", "--json"], {
+    cwd: root,
+  });
+  if (pnpmListResult.status !== 0) {
+    throw new Error(
+      `Failed to run pnpm list envio --json. Error: ${pnpmListResult.stderr}`
+    );
+  }
+  let pnpmList;
+  try {
+    const outputString = pnpmListResult.output.toString();
+    // It starts and ends with , so we need to remove it before parsing
+    const jsonString = outputString.slice(1, -1);
+    pnpmList = JSON.parse(jsonString);
+  } catch (e) {
+    throw new Error(
+      `Invalid pnpm list envio --json output. Error: ${e.message}`
+    );
+  }
+
+  let envioVersion;
+  try {
+    envioVersion = pnpmList[0].dependencies["envio"].version;
+  } catch (e) {
+    throw new Error(
+      `Failed to get envio version from pnpm list envio --json output. Error: ${e.message}. Pnpm list: ${pnpmList}`
+    );
+  }
+
+  if (!envioVersion.startsWith("file:")) {
+    throw new Error(
+      `Unexpected envio version. It should have the file: protocol. Actual: ${envioVersion}`
+    );
+  }
+
+  // It should be correctly set by lenvio init command,
+  // so we can rely on it to find a path to the local repository.
+  // We can't use the actual path from the node_modules folder,
+  // because it's stored in the global .pnpm and we can't get a path to binary using it.
+  const relativeLocalEnvioPath = envioVersion.replace("file:", "");
+
   const processResult = spawnSync(
     "cargo",
     [
       "run",
       "--manifest-path",
-      path.join(__dirname, "../../Cargo.toml"),
+      path.join(relativeLocalEnvioPath, "../../Cargo.toml"),
       ...args,
     ],
     { stdio: "inherit" }
