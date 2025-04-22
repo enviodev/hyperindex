a5173d1a127b9a6e66b5ca47efad108880c4a500
Dmitry Zakharov <dzakh.dev@gmail.com>
2025-04-22 17:44:12 +0400

    Move prometheus bindings to npm package

---
+++ codegenerator/cli/npm/envio/src/bindings/BigDecimal.gen.ts
@@ -0,0 +1,8 @@
+/* TypeScript file generated from BigDecimal.res by genType. */
+
+/* eslint-disable */
+/* tslint:disable */
+
+import type {default as $$t} from 'bignumber.js';
+
+export type t = $$t;
