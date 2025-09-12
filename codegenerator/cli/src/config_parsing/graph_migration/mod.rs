a8b07e244a4287b69b2228574e7f53705d6c8547
Jason <jason@envio.dev>
2025-09-12 12:39:43 +0200

    Move to enum config `address_config`

--- codegenerator/cli/src/config_parsing/graph_migration/mod.rs
+++ codegenerator/cli/src/config_parsing/graph_migration/mod.rs
@@ -285,7 +285,7 @@ pub async fn generate_config_from_subgraph_id(
         field_selection: None,
         raw_events: None,
         preload_handlers: Some(true),
-        lowercase_addresses: None,
+        address_format: None,
     };
     let mut networks: Vec<Network> = vec![];
 
