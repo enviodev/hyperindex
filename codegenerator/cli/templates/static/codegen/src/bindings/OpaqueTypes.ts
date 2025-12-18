d6fdffc11954a8e29927f5455c0d0394263edfed
Dmitry Zakharov <dzakh.dev@gmail.com>
2025-12-18 17:44:25 +0400

    Remove deprecated types

--- codegenerator/cli/templates/static/codegen/src/bindings/OpaqueTypes.ts
+++ codegenerator/cli/templates/static/codegen/src/bindings/OpaqueTypes.ts
@@ -1,5 +1,2 @@
-export type EthersAddress = string;
-export type Address = string;
-export type Nullable<T> = null | T;
 export type SingleOrMultiple<T> = T | T[];
 export type HandlerWithOptions<Fn, Opts> = (fn: Fn, opt?: Opts) => void;
